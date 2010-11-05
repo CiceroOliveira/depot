79f8fc64b0da7a99ccda3d5f18a33233081760da
Cicero Oliveira <cicero@cicerooliveira.com>
2010-11-04 22:59:30 -0400

    Task E finished.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @count = increment_count
   end
-
+  
+  private
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+  end
 end
